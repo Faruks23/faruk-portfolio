327a6befb326447db5fd6772337603393328694c
Faruks23 <mdfaruksp2@gmail.com>
2024-09-24 00:10:27 +0600

    change portfolio section

--- src/app/Components/Home/PortFolio/PortFolio.tsx
+++ src/app/Components/Home/PortFolio/PortFolio.tsx
@@ -1,7 +1,7 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import SectionTitle from '../../Common/SectionTitle/SectionTitle';
-import ProtCard from './ProtCard';
+
 import PortFolioSlider from './PortFolioSlider/PortFolioSlider';
 
 
@@ -16,13 +16,6 @@ const PortFolio = () => {
       <div className="prot-title flex justify-center !text-center">
         <SectionTitle title='My Portfolio' subtitle='VISIT MY PORTFOLIO'></SectionTitle>   
       </div>
-      {/* <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16 md:mt-20 gap-10">
-        {PortFolio.map((item,index )=> {
-          return <ProtCard item={item} key={index+1} />
-        
-        })}
-        
-      </div> */}
 
       <PortFolioSlider></PortFolioSlider>
 
