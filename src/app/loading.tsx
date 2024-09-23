c2ae2d4637a8dd779cdf41c7081986e1272a8d2e
Faruks23 <mdfaruksp2@gmail.com>
2024-09-24 00:52:28 +0600

    change portfolio section

---
+++ src/app/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return <div className=" h-screen w-full flex justify-center items-center bg-black text-white"> <p>Loading...</p></div>
+}
